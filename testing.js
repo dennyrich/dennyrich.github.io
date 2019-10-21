fd2c5e97c71d49b0a40ceebe383882ec9d038fab
Denny Rich <dennyrich215@gmail.com>
2019-10-21 16:33:51 -0700

    learning and testing

---
+++ testing.js
@@ -0,0 +1,6 @@
+class test extends React.Component {
+  render() {
+    return (<h1>Heading example</h1>)
+  }
+}
+ReactDOM.render(React.createElement("testing"), document.querySelector('#testId'));
