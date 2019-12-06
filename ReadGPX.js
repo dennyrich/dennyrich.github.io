f34978edaf798b31fe4d95bdc6626e38c61dafd4
Denny Rich <dennyrich215@gmail.com>
2019-12-05 20:36:35 -0800

    js practice file upload

--- ReadGPX.js
+++ ReadGPX.js
@@ -1,4 +1,23 @@
-function myMap() {
-	var mapProp= {center:new google.maps.LatLng(51.508742,-0.120850), zoom:5};
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+function read() {
+	var gpxFile = document.getElementById("GPX_file");
+	var name = gpxFile.name;
+	alert(gpxFile.value.concat(" file len: ", gpxFile.len));
+
+	//	
+	var request = new XMLHttpRequest();
+	
+	request.open("GET", "testXML.xml", false);
+	request.send();
+	var xmlData = request.responseXML;
+	//if no response xml
+	if (!xmlData) {
+		xmlData = (new DOMParser()).parseFromString(request.responseText, "text/xml");
+	}
+	var names = xmlData.getElementsByTagName("denny")[0].childNodes[0].nodeValue;
+	alert(names);
+
 }
+
+
+
