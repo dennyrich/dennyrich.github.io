f057b369f8a70d61acd54ed00f226ac26afb0463
Denny Rich <dennyrich215@gmail.com>
2019-12-22 21:54:17 -0800

    example

--- ReadGPX.js
+++ ReadGPX.js
@@ -1,18 +1,38 @@
 
-function read() {
+
+function read(gpxFile) {
 	document.getElementById("header").innerHTML = "File Uploaded..."
-	const gpxFile = document.getElementById("GPX_file").files[0];
-	var name = gpxFile.name;
-	alert("file name: " + name + " file size: " + gpxFile.size);
+	//const gpxFile = document.getElementById("GPX_file").files[0];
+	let text;
+	let xmlText;
+	if (!gpxFile) {
+		//use example file
+		const request = new XMLHttpRequest();
+		request.open('GET', '/Afternoon_Run.gpx', false);
+		request.send(null);
+		text = request.responseText;
+		xmlText = request.responseXML;
+		parseGPX(xmlText);
+	} else {
+		//using user file
+		const reader = new FileReader();	
+		reader.onloadend = function () {
+			text = reader.result;
+			parseGPX(text);
+		};
+		reader.readAsText(gpxFile);
+	}
+}
 
-	let parser = new DOMParser();
-
-
-
-	
-
-
+function parseGPX(text) {
+	console.log("done");
 }
 
 
-
+function myFun(callback) {
+	setTimeout(() => {
+		const result = 5;
+		console.log('finished');
+		callback(result);
+	}, 5000);
+}
