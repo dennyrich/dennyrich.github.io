7470d16884ea716a863ee3dce51d061df0158fd3
Denny Rich <dennyrich215@gmail.com>
2020-01-21 21:06:14 -0800

    changed name

--- ReadGPX.js
+++ ReadGPX.js
@@ -3,6 +3,12 @@ var world = Array();
 var swBound; //southwest bound for map
 var neBound; //northeast 
 var bound; //google maps bound 
+var segments = Array();
+const HEIGHT = 300; //must change canvas element if change height/width
+const WIDTH = 300;
+
+//const canvas = document.getElementById("canvas");
+var ctx;
 
 function myMap() {
   var mapProp= {
@@ -28,10 +34,9 @@ function drawMap() {
 	map.fitBounds(bound);
 	
 }
-//**************************************************************************
+//**************************************************************************/
 //file name is null if file is uploaded
 function read(gpxFile, fileName) {
-	document.getElementById("header").innerHTML = "File Uploaded..."
 	//const gpxFile = document.getElementById("GPX_file").files[0];
 	let text;
 	let xmlText;
@@ -55,6 +60,7 @@ function read(gpxFile, fileName) {
 		};
 		reader.readAsText(gpxFile);
 	}
+	document.getElementById("header").innerHTML = "File Uploaded..."
 }
 
 //creates data elements from xml (.gpx) doc and calls createWorld
@@ -108,13 +114,131 @@ function createWorld(trkseg) {
 		index++;
 	}
 	bound = new google.maps.LatLngBounds(new google.maps.LatLng(southmost, eastmost), new google.maps.LatLng(northmost, westmost));
+	createSegments(world);
+}
+//***************************************************************************** */
+
+//
+class Vector {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	magnitude() {
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+	static angleBetween(v1, v2) {
+		//const cosAngle =  (v1.x * v2.x + v1.y * v2.y) / (v1.magnitude() * v2.magnitude());
+		//(x1*y2-y1*x2,x1*x2+y1*y2);
+		return  Math.atan((v1.x * v2.y - v1.y * v2.x) / (v1.x * v2.x + v1.y * v2.y));
+	}
 }
 
+class Polygon {
+	//all Vectors
+	constructor(bLeft, bRight, tLeft, tRight) {
+		this.bLeft = bLeft;
+		this.bRight = bRight;
+		this.tLeft = tLeft;
+		this.tRight = tRight;
+	}
+	getPolyAbove(curve, len, dWidth) {
+		const bLeft = this.tLeft;
+		const bRight = this.tRight;
+		const tLeft = new Vector(this.tLeft.x + dWidth + curve, this.tLeft.y + len);
+		const tRight = new Vector(this.tRight.x - dWidth + curve, this.tRight.y + len);
+		return new Polygon(bLeft, bRight, tLeft, tRight);
+	}
+}
 
-// function myFun(callback) {
-// 	setTimeout(() => {
-// 		const result = 5;
-// 		console.log('finished');
-// 		callback(result);
-// 	}, 5000);
-//}
+// class BillBoard() {
+
+// }
+function createSegments(world) {
+	var distX;
+	var distY;
+	var distElev;
+	var currVec;
+	var prevVec;
+	//var prevDistX = 0;
+	//var prevDistY = 0;
+	var angleFromPrev;
+	var prevVec = new Vector(0, 0);
+	const scale = 1000;
+	const elevScale = 10;
+	//var segPosY = HEIGHT;
+	for (let i = 1; i < world.length; i++) {
+		distX = world[i][0] - world[i - 1][0];
+		distY = world[i][1] - world[i - 1][1];
+		distElev = world[i][2] - world[i - 1][2];
+		currVec = new Vector(distX, distY);
+		//angleFromPrev = Vector.angleBetween();
+
+		var len = scale * currVec.magnitude();
+		angleFromPrev = Vector.angleBetween(currVec, prevVec); //order matters, check sign
+		var curve;
+		if (Math.abs(angleFromPrev) > Math.PI) {
+			curve = 4 * Math.sign(angleFromPrev);
+		} else if (Math.abs(angleFromPrev) > Math.PI / 2) {
+			curve = 3 * Math.sign(angleFromPrev);
+		} else if (Math.abs(angleFromPrev > Math.PI/4)) {
+			curve = 2 * Math.sign(angleFromPrev);
+		} else if (Math.abs(angleFromPrev) > Math.PI/16) {
+			curve = Math.sign(angleFromPrev);
+		} else { //includes NaN when delta x = 0
+			curve = 0;
+		}
+
+		var changeElevScaled = distElev * elevScale;
+		len += changeElevScaled;
+		//world = list of [lat, lon, elev, time]
+		segments.push({
+			length : len,
+			curveAmount : curve
+			//changeElev : changeElevScaled
+		});
+
+		prevVec = currVec;
+	}
+} 
+
+//********************************************************************************** */
+function run() {
+	const canvas = document.getElementById("canvas");
+	ctx = canvas.getContext('2d');
+
+
+	
+
+
+
+	//testing
+	var p = new Polygon(new Vector(100, HEIGHT), new Vector(200, HEIGHT), new Vector(125, HEIGHT - 100), new Vector(175, HEIGHT - 100));
+	drawPoly(p);
+}
+
+function frame(polyList, offsetIntoFirst) {
+	for (const p in polyList) {
+		drawPoly(p);
+	}
+}
+
+function drawPoly(polygon) {
+	const bRight = polygon.bRight;
+	const bLeft = polygon.bLeft;
+	const tRight = polygon.tRight;
+	const tLeft = polygon.tLeft;
+	ctx.moveTo(bRight.x, bRight.y);
+	ctx.beginPath();
+	ctx.lineTo(bLeft.x, bLeft.y);
+	ctx.lineTo(tLeft.x, tLeft.y);
+	ctx.lineTo(tRight.x, tRight.y);
+	ctx.lineTo(bRight.x, bRight.y);
+	ctx.fill();
+}
+//*************************************************** */
+
+function runner() {
+
+
+}
